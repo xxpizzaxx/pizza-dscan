d988f542dcdae9b13d8caac548a9a264996bf9cd
Andi Miller <andi@andimiller.net>
2015-12-16 03:54:17 +0000

    remove getTypeGroups, change groups to be a Map for lookup purposes

--- src/main/scala/DBOps.scala
+++ src/main/scala/DBOps.scala
@@ -8,15 +8,10 @@ import scala.concurrent.duration._
  */
 class DBOps(db: JdbcBackend.Database) {
 
-  def getTypeGroups(): Unit = {
-    Tables.Invtypes.join(Tables.Invgroups)
-  }
-
   val groupsQuery = for {
     ((t, g), c) <- Tables.Invtypes join Tables.Invgroups on (_.groupid === _.groupid) join Tables.Invcategories on (_._2.categoryid === _.categoryid)
   } yield (t.typename, t.typeid, g.groupname, c.categoryname)
 
   case class GroupData(typeId: Int, typeName: String, groupName: Option[String], categoryName: Option[String])
-  lazy val groups = groupsQuery.result.run(db).sync(60 seconds).filter(_._1.nonEmpty).map(t => GroupData(t._2, t._1.get, t._3, t._4))
-
+  lazy val groups = groupsQuery.result.run(db).sync(60 seconds).filter(_._1.nonEmpty).map(t => GroupData(t._2, t._1.get, t._3, t._4)).map(gd => (gd.typeName, gd)).toMap
 }
